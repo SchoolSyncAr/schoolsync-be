fa0b96cf103b16ed484fc385c7551adf23922560
madescoces <madescoces@gmail.com>
2024-05-29 07:52:26 -0300

    Moficación completa a datos en token, se agrega expiración del token

--- src/main/kotlin/dto/auth/AuthenticationResponse.kt
+++ src/main/kotlin/dto/auth/AuthenticationResponse.kt
@@ -3,7 +3,5 @@ package ar.org.schoolsync.dto.auth
 import ar.org.schoolsync.model.Role
 
 data class AuthenticationResponse(
-    val accessToken: String,
-    val role: Role,
-    val userId: Long
+    val accessToken: String
 )
