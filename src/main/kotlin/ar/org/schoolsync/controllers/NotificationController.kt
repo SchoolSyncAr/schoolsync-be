3275a7443010a5d63bcdfff280c1b4df73aeb369
francopolesel <francopolesel99@gmail.com>
2024-06-22 02:02:12 -0300

    Now you can search notifications by children and unread

--- src/main/kotlin/ar/org/schoolsync/controllers/NotificationController.kt
+++ src/main/kotlin/ar/org/schoolsync/controllers/NotificationController.kt
@@ -39,6 +39,19 @@ class NotificationController(
     }
 
     @RolesAllowed("USER")
+    @GetMapping("/allByUser")
+    @Operation(summary = "Retorna todas las notificaciones de un usuario")
+    fun findAll(
+        @RequestParam searchField: String,
+        @RequestParam sortField: String,
+        @RequestParam unreadOnly: Boolean,
+        @RequestParam parentId: Long,
+        @RequestParam childrenId: Long?,
+    ): List<NotificationDTO> {
+        return notificationService.findAllByUser(SearchFilter(searchField, sortField), unreadOnly, parentId, childrenId)
+    }
+
+    @RolesAllowed("USER")
     @GetMapping("/count")
     fun getUnreadNotificationsCount(@RequestParam userId: Long): Int {
         return notificationRegistryService.getUnreadNotificationsCount(userId)
--- src/main/kotlin/ar/org/schoolsync/services/NotificationService.kt
+++ src/main/kotlin/ar/org/schoolsync/services/NotificationService.kt
@@ -8,6 +8,7 @@ import ar.org.schoolsync.model.Notification
 import ar.org.schoolsync.model.SearchFilter
 import ar.org.schoolsync.model.SearchFilterBuilder
 import ar.org.schoolsync.model.enums.Status
+import ar.org.schoolsync.repositories.NotificationRegistryRepository
 import ar.org.schoolsync.repositories.NotificationRepository
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
@@ -15,7 +16,9 @@ import kotlin.jvm.optionals.getOrNull
 
 @Service
 class NotificationService(
-    @Autowired private val notificationRepository: NotificationRepository
+    @Autowired private val notificationRepository: NotificationRepository,
+    @Autowired private val notificationRegistryRepository: NotificationRegistryRepository,
+    @Autowired private val userService: UserService,
 ) {
     fun findByID(id: Long): Notification? = notificationRepository.findById(id).getOrNull()
     fun findOrErrorByID(id: Long): Notification =
@@ -30,6 +33,33 @@ class NotificationService(
             .build()
 
         return notificationRepository.findAll(spec, filter.getSortAdmin()).map { it.toAdminResponse() }
+    }
+
+    fun findAllByUser(filter: SearchFilter, unreadOnly: Boolean, parentId: Long, childrenId: Long?): List<NotificationDTO>{
+        val spec = SearchFilterBuilder(filter)
+            .from()
+            .title()
+            .content()
+            .active()
+            .build()
+
+        var childrenIds = mutableListOf<Long>()
+        childrenIds.add(parentId)
+        childrenIds.addAll(userService.findMyChildren(parentId).map { it.id })
+        if (childrenId != null){
+            childrenIds = childrenIds.filter { it == childrenId }.toMutableList()
+        }
+        var notifications = notificationRegistryRepository.findAll().filter { notification ->
+            childrenIds.any { it == notification.receiver.id }
+        }
+        if (unreadOnly) {
+            notifications = notifications.filter { !it.read }
+        }
+        return notificationRepository.findAll(spec, filter.getSortAdmin())
+            .filter { notification ->
+                notifications.any { it.notification.id == notification.id }
+            }
+            .map { it.toAdminResponse() }
     }
 
     fun deleteById(id: Long) {
